71bd71170f3d5866dae1d4ffe1ba89e96df054d5
Ivan Uvalle <ivanniquilador1@gmail.com>
2026-09-24 19:59:14 -0600

    fix: corregir prueba unitaria

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
 test('Prueba unitaria: sumar dos numeros positivos', () => {
   const resultado = sumar(2, 3);
-  assert.strictEqual(resultado, 10); // Esto fallará intencionalmente
+  assert.strictEqual(resultado, 5); // Corregido
 });
